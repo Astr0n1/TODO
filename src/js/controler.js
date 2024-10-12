913dbf7d758fec01baf650381ac38fb4c55c1c64
Astr0n1 <astron0learn@gmail.com>
2024-10-13 02:55:46 +0300

    mobile version , drag and drop

--- src/js/controler.js
+++ src/js/controler.js
@@ -5,6 +5,7 @@ const formChecked = document.querySelector(".create__checkbox");
 const formContent = document.querySelector(".create__input");
 const listDOM = document.querySelector(".list");
 const control = document.querySelector(".control");
+const clearCompleted = document.querySelector("a.clear");
 const taskNumber = document.querySelector(".left");
 
 // global variables
@@ -36,7 +37,7 @@ listDOM.addEventListener("click", (e) => {
   removeTask(task);
 });
 
-// check task
+// mark task comlete
 listDOM.addEventListener("click", (e) => {
   const checkbox = e.target.closest(`input[type='checkbox']`);
   const task = e.target.closest(".task");
@@ -44,6 +45,8 @@ listDOM.addEventListener("click", (e) => {
   // guard class
   if (!task || !checkbox) return;
   task.classList.toggle("completed");
+
+  filterList();
 });
 
 // filter tasks
@@ -53,8 +56,20 @@ control.addEventListener("click", (e) => {
   // guard class
   if (!target) return;
 
+  // update current filter
+  control.querySelectorAll("a").forEach((el) => el.classList.remove("current"));
+  target.classList.add("current");
+
   filter = target.textContent.toLowerCase();
   filterList();
+});
+
+// clear complete
+clearCompleted.addEventListener("click", () => {
+  const completedTasks = tasks.filter((task) =>
+    task.classList.contains("completed")
+  );
+  completedTasks.forEach((task) => removeTask(task));
 });
 
 /////////////////////////////////////////////////////////
@@ -85,12 +100,14 @@ const addNewTask = function () {
   `;
   listDOM.insertAdjacentHTML("afterbegin", markup);
   updateArray();
+  filterList();
 };
 
 const removeTask = function (task) {
   const index = tasks.indexOf(task);
   tasks.splice(index, 1);
   updateListDOM();
+  filterList();
 };
 
 const renderTask = function (task) {
@@ -111,9 +128,9 @@ const updateListDOM = function () {
 
 const filterList = function () {
   clear();
+  let tasksNum = 0;
   tasks.reverse().forEach((task) => {
     const complete = task.classList.contains("completed");
-    let tasksNum = 0;
     if (
       filter === "all" ||
       (filter === "active" && !complete) ||
