354214336702bf18ff6a1ce7daad9584cb6a7cf5
Astr0n1 <astron0learn@gmail.com>
2024-10-12 15:57:13 +0300

    rebuild (css done)

--- src/js/controler.js
+++ src/js/controler.js
@@ -1,24 +1,6 @@
-import { view, todo } from "./view.js";
-import * as model from "./model.js";
-////////////////////////////////////////////////
-const updateLocalStorage = function (tasksList) {
-  model.storeLocal(tasksList);
-};
-const restoreLocalStorage = function () {
-  return model.restoreLocal();
-};
+const themeBtn = document.querySelector(".theme");
 
-////////////////////////////////////////////////
-const init = function () {
-  todo.restoreLocal(model.restoreLocal);
-
-  view.addHandlerTheme();
-
-  todo.addHandlerNewTask(updateLocalStorage);
-  todo.addHandlerRemoveTask(updateLocalStorage);
-  todo.addHandlerMarkComplete(updateLocalStorage);
-  todo.addHandlerClearComplete(updateLocalStorage);
-  todo.addHandlerFilter();
-};
-
-init();
+themeBtn.addEventListener("click", () => {
+  console.log("click");
+  document.body.classList.toggle("dark");
+});
--- src/js/model.js
+++
@@ -1,9 +0,0 @@
-export const storeLocal = function (taskList) {
-  localStorage.setItem("task-list", JSON.stringify(taskList));
-};
-
-export const restoreLocal = function () {
-  const items = JSON.parse(localStorage.getItem("task-list"));
-  if (!items) return;
-  return items;
-};
